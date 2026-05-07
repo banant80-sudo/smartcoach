b3f90e61c6823dc7918fd5eef8e771a5f291d647
banant80-sudo <banant80@gmail.com>
2026-05-08 00:33:54 +0530

    Add MainActivity for app navigation

---
+++ android/app/src/main/java/com/smartcoach/airobotteacher/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.smartcoach.airobotteacher
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Setup bottom navigation
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav)
+        val navController = findNavController(R.id.nav_host_fragment)
+
+        NavigationUI.setupWithNavController(bottomNav, navController)
+
+        // Set toolbar
+        setSupportActionBar(findViewById(R.id.toolbar))
+    }
+}
